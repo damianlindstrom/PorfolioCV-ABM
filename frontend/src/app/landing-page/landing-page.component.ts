ea15d4490c86cda02108963e624870a051912239
Damian <damianlindstrom@gmail.com>
2025-04-12 00:34:35 -0300

    Reparación de links

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router'; 
+
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
